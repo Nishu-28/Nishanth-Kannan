e6fc0243af9daabf7ff1d1c8aa3e3ac93ea89a42
Nishanth Kannan <nishu28n18v7d@gmail.com>
2025-05-01 23:47:48 +0530

    Logo and stuffs

--- script.js
+++ script.js
@@ -25,6 +25,15 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+window.addEventListener('scroll', function () {
+    const header = document.querySelector('header');
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
+
 
 // Function to add active class to the current section's link
 function addActiveClassToLink(id) {
